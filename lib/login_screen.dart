f15427b651c59cdb0b3006a9c14f0ebb1280cf24
alayyubisind <alayyubisind@student.ub.ac.id>
2025-09-22 12:42:03 +0700

    tes 1

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -4,8 +4,18 @@ void main() {
   runApp(LoginScreen());
 }
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _obscurePassword = true; //tooglepass
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +35,25 @@ class LoginScreen extends StatelessWidget {
                   width: isTablet ? 600 : (isPortrait ? screenWidth * 0.9 : screenWidth * 0.65),
                   padding: EdgeInsets.all(16),
                   height: screenHeight,
-                  child: Column(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 30),
+                      Text(
+                        "Masuk Di Sini",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "Selamat Datang Kembali!",
+                        style: TextStyle(fontSize:16,color: Colors.black54),
+                      )
+                    ],
+                  ),
                 );
               }),
             );
@@ -34,4 +62,43 @@ class LoginScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+
+// Widget untuk tampilan  lanskap
+Widget _buildLanscape(double screenWidth) {
+  return Container(
+    color: Colors.blueGrey[50],
+    child: Center(
+      child: Text(
+        "Landscape UI",
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      ),
+    ),
+  );
+}
+
+// Widget untuk tampilan potret
+Widget _buildPortrait(double screenWidth) {
+  return Container(
+    color: Colors.teal[50],
+    child: Center(
+      child: Text(
+        "Portrait UI",
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      ),
+    ),
+  );
+}
+
+Widget _buildDesktop(double screenWidth) {
+  return Container(
+    color: Colors.amber[50],
+    child: Center(
+      child: Text(
+        "Desktop UI",
+        style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+      ),
+    ),
+  );
 }
