ee92b5c9f7e9e814011df380f407677220ec638a
shamhasan <ihtishamulhasaaan@gmail.com>
2025-09-22 10:02:10 +0700

    3 ui screen, landscape, portrait, desktop

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(LoginScreen());
+  runApp(MaterialApp(home: LoginScreen()));
 }
 
 class LoginScreen extends StatelessWidget {
@@ -11,7 +11,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    bool isTablet = screenWidth > 600; // Deteksi jika perangkat adalah tablet
+    bool isDesktop = screenWidth > 960; // Deteksi jika perangkat adalah Desktop
 
     return Scaffold(
       body: SafeArea(
@@ -20,16 +20,62 @@ class LoginScreen extends StatelessWidget {
             bool isPortrait = orientation == Orientation.portrait;
 
             return Center(
-              child: LayoutBuilder(builder: (context, builder){
-                return Container(
-                  width: isTablet ? 600 : (isPortrait ? screenWidth * 0.9 : screenWidth * 0.65),
-                  padding: EdgeInsets.all(16),
-                  height: screenHeight,
-                  child: Column(),
-                );
-              }),
+              child: LayoutBuilder(
+                builder: (context, builder) {
+                  return Container(
+                    width: isDesktop
+                        ? 960
+                        : (isPortrait ? screenWidth * 0.9 : screenWidth * 0.65),
+                    padding: EdgeInsets.all(16),
+                    height: screenHeight,
+                    child: isDesktop
+                        ? _buildDesktop(screenWidth)
+                        : (isPortrait
+                              ? _buildPortrait(screenWidth)
+                              : _buildLanscape(screenWidth)),
+                  );
+                },
+              ),
             );
           },
+        ),
+      ),
+    );
+  }
+
+  // Widget untuk tampilan lanskap
+  Widget _buildLanscape(double screenWidth) {
+    return Container(
+      color: Colors.blueGrey[50],
+      child: Center(
+        child: Text(
+          "Landscape UI",
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  // Widget untuk tampilan potret
+  Widget _buildPortrait(double screenWidth) {
+    return Container(
+      color: Colors.teal[50],
+      child: Center(
+        child: Text(
+          "Portrait UI",
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDesktop(double screenWidth) {
+    return Container(
+      color: Colors.amber[50],
+      child: Center(
+        child: Text(
+          "Desktop UI",
+          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
         ),
       ),
     );
