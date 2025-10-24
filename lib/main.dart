c3e666b4f5c3cab5a8f83f9c427d9ac5ef07b061
shamhasan <ihtishamulhasaaan@gmail.com>
2025-10-25 02:46:22 +0700

    implementation: Supabase, add:auth_service.dart, supabase sudah terinstall

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,25 @@ import 'package:multi_kelompok/home_screen.dart';
 import 'package:multi_kelompok/login_screen.dart';
 import 'package:multi_kelompok/popular_movie_ui.dart';
 import 'package:multi_kelompok/watchlist.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: "https://vafjsmuirzjuwvuocpwq.supabase.co",
+    anonKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhZmpzbXVpcnpqdXd2dW9jcHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNDcwMTAsImV4cCI6MjA3NjgyMzAxMH0.epwC9GtxSQxb3PvVIqCWFuKNwoVti9okPqPUl-dLLWg",
+  );
+
+
   runApp(
     MaterialApp(
-      home: 
-      // HomeScreen()
-      // WatchlistPage()
-      // PopularMoviesPage()
-      // GenreListPage()
-      LoginScreen()
-      ,
-      debugShowCheckedModeBanner: false
-      )
-    );
+      home:
+          // HomeScreen()
+          // WatchlistPage()
+          // PopularMoviesPage()
+          // GenreListPage()
+          LoginScreen(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
