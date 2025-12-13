446944eca37d2a46d41fe3fc6f8c214583b73a90
egojgd <kardusbobrok@gmail.com>
2025-12-14 02:01:31 +0700

    COMMIT

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
+
 import 'package:flutter/material.dart';
-import 'package:multi_kelompok/daftar_genre.dart';
-import 'package:multi_kelompok/home_screen.dart';
-import 'package:multi_kelompok/login_screen.dart';
-import 'package:multi_kelompok/popular_movie_ui.dart';
-import 'package:multi_kelompok/watchlist.dart';
-import 'package:multi_kelompok/admin/admin_screen.dart';
-import 'package:multi_kelompok/admin/genre_admin_screen.dart';
-import 'package:multi_kelompok/admin/movie_admin_screen.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter/material.dart';
-import 'package:multi_kelompok/providers/review_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'package:multi_kelompok/Providers/MovieProvider.dart';
+import 'package:multi_kelompok/Providers/auth_provider/AuthProvider.dart';
+import 'package:multi_kelompok/providers/genre_provider.dart';
+import 'package:multi_kelompok/widgets/auth/AuthGate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,12 +14,17 @@ void main() async {
   await Supabase.initialize(
     url: "https://vafjsmuirzjuwvuocpwq.supabase.co",
     anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhZmpzbXVpcnpqdXd2dW9jcHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNDcwMTAsImV4cCI6MjA3NjgyMzAxMH0.epwC9GtxSQxb3PvVIqCWFuKNwoVti9okPqPUl-dLLWg",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhZmpzbXVpcnpqdXd2dW9jcHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNDcwMTAsImV4cCI6MjA3NjgyMzAxMH0.epwC9GtxSQxb3PvVIqCWFuKNwoVti9okPqPUl-dLLWg",
   );
 
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => ReviewProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => MovieProvider()),
+        ChangeNotifierProvider(create: (_) => GenreProvider()),
+
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home:
@@ -31,10 +32,11 @@ void main() async {
         // WatchlistPage()
         // PopularMoviesPage()
         // GenreListPage()
-         // LoginScreen(),
+        // LoginScreen(),
         // MovieAdminScreen(),
-        GenreAdminPage()
+        // GenreAdminScreen()
         // AdminScreen()
+        AuthGate(),
       ),
     ),
   );
