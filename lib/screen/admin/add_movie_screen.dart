8f3e3656fb823ede2359f163c7c228b1ae836601
Daniel <irfan2904.ia@gmail.com>
2025-12-17 21:51:40 +0700

    benerin dikit

--- lib/screen/admin/add_movie_screen.dart
+++ lib/screen/admin/add_movie_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:multi_kelompok/Providers/MovieProvider.dart'; // Sesuaikan path ini
+import 'package:multi_kelompok/providers/MovieProvider.dart'; // Sesuaikan path ini
 import 'package:multi_kelompok/models/movie_model.dart';
 
 import '../../providers/genre_provider.dart'; // Sesuaikan path ini
